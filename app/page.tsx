8df81ede4244c6172550df7b415bdccc11f4f130
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2024-11-25 00:17:19 +0300

    Chat Service | Update

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   const onClickHandle = async () => {
     try {
       setError(null); // Hataları sıfırla
-      const res = await fetch("http://localhost:3001/api/eleman/login", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/eleman/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
